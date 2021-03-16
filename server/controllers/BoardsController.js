fda16cd3cec0293b7faf7f87d492407f95e70ce9
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-16 13:53:46 -0600

    boards are done

--- server/controllers/BoardsController.js
+++ server/controllers/BoardsController.js
@@ -8,10 +8,10 @@ export class BoardsController extends BaseController {
     this.router
     // .get('/:id/lists', this.getListsByBoardId)
       .use(Auth0Provider.getAuthorizedUserInfo)
-      .get('/:creatorId', this.getBoardById)
+      .get('/:id', this.getBoardById)
       .get('/', this.getUserBoards)
       .post('', this.createBoard)
-      // .put('/:id', this.editBoard)
+      .put('/:id', this.editBoard)
       .delete('/:id', this.deleteBoard)
   }
 
@@ -35,6 +35,15 @@ export class BoardsController extends BaseController {
   async getBoardById(req, res, next) {
     try {
       return res.send(await boardsService.getBoardById(req.params.id))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editBoard(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      res.send(await boardsService.editBoard(req.params.id, req.userInfo.id, req.body))
     } catch (error) {
       next(error)
     }
--- server/services/BoardsService.js
+++ server/services/BoardsService.js
@@ -13,10 +13,17 @@ class BoardsService {
     return await dbContext.Board.findById(query)
   }
 
+  async editBoard(id, userId, body) {
+    const post = await dbContext.Board.findOneAndUpdate({ _id: id, creatorId: userId }, body, { new: true })
+    if (!post) {
+      throw new BadRequest('You are not the CREATOR or BAD ID.')
+    }
+  }
+
   async deleteBoard(id, userId) {
     const post = await dbContext.Board.findOneAndRemove({ _id: id, creatorId: userId })
     if (!post) {
-      throw new BadRequest('You are not the creator or Bad Id.')
+      throw new BadRequest('You are not the CREATOR or BAD ID.')
     }
   }
 }
